b523346c914bcc45db2d8ab802e359b59cd3d1c7
Kaburu12 <michael.kaburur@student.moringaschool.com>
2022-06-17 14:16:06 +0300

    commit tree f9da66091368d0c8dab7d309ac867f15f5ccedb1

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+  let l = word.length;
+  for (let i = 0; i < l/2; i++) {
+   if (word[i] !== word[l - 1 - i]) {
+    return false;
+   }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  write a function called is palindrome & pass one parameter
+  use a for loop to check to iterate over the characters of the word
+  use an if statement to check if the first and the last characters of the word if they match
+  if they match === the word is palindrome (true)
+  else !===palindrome(false)
 */
 
 /*
   Add written explanation of your solution here
+  when a string is passed through the function, and the word is iterated over by the for loop ,the if statement checks the comparison of the first and last characters if they match is true(palindrome) and if  they don't is false(not palindrome)
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("deed"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("deeds"));
 }
 
 module.exports = isPalindrome;
